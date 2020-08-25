ff2aa5dd6f8d75e45b938c42fcd118cd8869cc2f
Kusuma Teluguntla <kteluguntla@gmail.com>
2020-08-25 16:41:58 -0700

    I slightly updated the profile screen

--- lib/Profile_Screen.dart
+++ lib/Profile_Screen.dart
@@ -1,9 +1,16 @@
-import 'package:flutter/material.dart';
 
+
+import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Future<Widget> build(BuildContext context) async {
+    return Container();
+  }
+}
 
